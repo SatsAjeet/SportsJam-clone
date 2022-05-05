16fa7e8876e919ad2335e200e986a83a6edebcbe
abhishekmardiya <mardiyaabhishek@gmail.com>
2022-05-05 13:00:27 +0530

    Section 2 added

--- Project/sportsjam/src/components/Homepage.jsx
+++ Project/sportsjam/src/components/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Carousel from './homepage/Carousel '
 import Section1 from './homepage/Section1'
+import Section2 from './homepage/Section2'
 
 const Homepage = () => {
   return (
@@ -8,6 +9,7 @@ const Homepage = () => {
       <div>
         <Carousel/>
         <Section1/>
+        <Section2/>
       </div>
     </div>
   )
