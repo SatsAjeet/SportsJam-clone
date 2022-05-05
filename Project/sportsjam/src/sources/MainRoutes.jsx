c7c77a4b99e7ad135f71735e1d873fb55129cf80
abhishekmardiya <mardiyaabhishek@gmail.com>
2022-05-05 22:48:30 +0530

    product routing connected

--- Project/sportsjam/src/sources/MainRoutes.jsx
+++ Project/sportsjam/src/sources/MainRoutes.jsx
@@ -1,35 +1,36 @@
-import React, { useEffect } from 'react'
-import {Route, Routes, useNavigate } from 'react-router-dom';
-import CreateAccount from '../components/CreateAccount';
-import Homepage from '../components/Homepage';
-import Navbar from '../components/Navbar'
-import Cart from '../components/cart';
-import SignIn from '../components/SignIn';
-import WishList from '../components/WishList';
-import ProductDescription from '../components/Prodcutdescription';
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import CreateAccount from "../components/CreateAccount";
+import Homepage from "../components/Homepage";
+import Navbar from "../components/Navbar";
+import Cart from "../components/Cart";
+import SignIn from "../components/SignIn";
+import WishList from "../components/WishList";
+import Product from "../components/product";
+
 
 const MainRoutes = () => {
-    //redirected to homepage everytime someone refresh or lands on the website
-    const navigate=useNavigate();
+  //redirected to homepage everytime someone refresh or lands on the website
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    navigate("/home")
-  },[]);
+  useEffect(() => {
+    navigate("/home");
+  }, []);
 
   return (
     <div>
-        <Navbar/>
+      <Navbar />
 
-        <Routes>
-            <Route path="home" element={<Homepage/>}/>
-            <Route path="signin" element={<SignIn/>}/>
-            <Route path="createaccount" element={<CreateAccount/>}/>
-            <Route path="wishlist" element={<WishList/>}/>
-            <Route path="cart" element={<Cart/>}/>
-            <Route path="/description" element={<ProductDescription/>}/>
-        </Routes>
+      <Routes>
+        <Route path="home" element={<Homepage />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="createaccount" element={<CreateAccount />} />
+        <Route path="wishlist" element={<WishList />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="/products" element={<Product/>}/>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
