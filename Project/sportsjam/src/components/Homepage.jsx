55dd4cf297169465947f9a8e6045f9e833122453
abhishekmardiya <mardiyaabhishek@gmail.com>
2022-05-06 12:44:05 +0530

    homepage completed

--- Project/sportsjam/src/components/Homepage.jsx
+++ Project/sportsjam/src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import Section1 from './homepage/Section1'
 import Section2 from './homepage/Section2'
 import Section3 from './homepage/Section3'
 import Corousel2 from './homepage/Corousel2'
+import Section4 from './homepage/Section4'
 
 
 const Homepage = () => {
@@ -15,6 +16,7 @@ const Homepage = () => {
         <Section2/>
         <Section3/>
         <Corousel2/>
+        <Section4/>
       </div>
     </div>
   )
