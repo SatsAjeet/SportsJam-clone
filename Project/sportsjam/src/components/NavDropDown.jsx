2bb8cc18d48b74c3a7a793969b9351084e0e58fd
SatsAjeet <98746450+SatsAjeet@users.noreply.github.com>
2022-05-07 22:37:06 +0530

    Update NavDropDown.jsx

--- Project/sportsjam/src/components/NavDropDown.jsx
+++ Project/sportsjam/src/components/NavDropDown.jsx
@@ -8,9 +8,9 @@ const NavDropDown = () => {
   }
   return (
     <div onClick={handleClick}>
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>SPORTS</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>SPORTS</p>
+        <div className={Styles.dropdowncontent}>
           <p>Badminton</p>
           <p>Basketball</p>
           <p>Boxing</p>
@@ -26,9 +26,9 @@ const NavDropDown = () => {
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>FITNESS</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>FITNESS</p>
+        <div className={Styles.dropdowncontent}>
           <p>Abs & Core</p>
           <p>Aerobics / Fitness</p>
           <p>Cardio</p>
@@ -39,9 +39,9 @@ const NavDropDown = () => {
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>SHOES</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>SHOES</p>
+        <div className={Styles.dropdowncontent}>
           <p>Men</p>
           <p>Women</p>
           <p>Junior</p>
@@ -49,42 +49,42 @@ const NavDropDown = () => {
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>CLOTHING</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>CLOTHING</p>
+        <div className={Styles.dropdowncontent}>
           <p>Men</p>
           <p>Women</p>
           <p>Junior</p>
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>ACCESSORIES</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>ACCESSORIES</p>
+        <div className={Styles.dropdowncontent}>
           <p>Sports Bags</p>
           <p>Accessories</p>
           <p>By Sports</p>
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>LEISURE SPORTS</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>LEISURE SPORTS</p>
+        <div className={Styles.dropdowncontent}>
           <p>Leisure Sports</p>
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>FANGEAR</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>FANGEAR</p>
+        <div className={Styles.dropdowncontent}>
           <p>Soccer Leagues</p>
           <p>Cricket</p>
         </div>
       </div>
 
-      <div class={Styles.dropdown}>
-        <p class={Styles.dropbtn}>BRANDS</p>
-        <div class={Styles.dropdowncontent}>
+      <div className={Styles.dropdown}>
+        <p className={Styles.dropbtn}>BRANDS</p>
+        <div className={Styles.dropdowncontent}>
           <p>Brands Of Your Choice</p>
         </div>
       </div>
