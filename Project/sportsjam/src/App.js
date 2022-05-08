4baf3b830b8a7c2e94108f29bdea7fb9c2a2a1fd
SatsAjeet <98746450+SatsAjeet@users.noreply.github.com>
2022-05-08 08:04:22 +0530

    signup some edit

--- Project/sportsjam/src/App.js
+++ Project/sportsjam/src/App.js
@@ -1,15 +1,8 @@
 import MainRoutes from './sources/MainRoutes';
-
-import "./App.css";
-//import Form from "./components/Form";
-import Signup from './components/Signup';
-
-
 function App() {
   
   return (
     <div className="App">
-      <Signup/>
       <MainRoutes />
     </div>
   );
